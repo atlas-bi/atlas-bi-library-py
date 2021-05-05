2bddbe813349b5a00e524400f9403aba8aab02e5
Christopher Pickering <cpickering@rhc.net>
2021-05-05 17:13:05 -0500

    updated precommit

--- poetry_to_requirements.py
+++ poetry_to_requirements.py
@@ -2,7 +2,7 @@ import argparse
 import subprocess
 from typing import Optional
 from typing import Sequence
-
+import os
 
 def main(argv: Optional[Sequence[str]] = None) -> int:
     parser = argparse.ArgumentParser(
@@ -10,7 +10,7 @@ def main(argv: Optional[Sequence[str]] = None) -> int:
     )
 
     check_cmd = ('poetry', 'export', '--without-hashes')
-    build_cmd = ('poetry', 'export', '--output', 'requirements.txt', '--without-hashes')
+    build_cmd = ('poetry', 'export', '--output', 'atlas/requirements.txt', '--without-hashes')
 
     # check current file
     proc = subprocess.Popen(check_cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
@@ -21,7 +21,7 @@ def main(argv: Optional[Sequence[str]] = None) -> int:
 
     retcode = 0
 
-    if open("requirements.txt", "r").read() != stdout:
+    if os.path.isfile("atlas/requirements.txt") and open("atlas/requirements.txt", "r").read() != stdout:
         subprocess.Popen(build_cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
         print('created new requirements.txt file')
         retcode = 1
