c7dbf57cd943d47e93757d69013f0cf8c1ac0c53
Christopher Pickering <cpickering@rhc.net>
2021-05-05 17:07:11 -0500

    added precommit hook to build requirements

---
+++ poetry_to_requirements.py
@@ -0,0 +1,32 @@
+import argparse
+import subprocess
+from typing import Optional
+from typing import Sequence
+
+
+def main(argv: Optional[Sequence[str]] = None) -> int:
+    parser = argparse.ArgumentParser(
+        'Convert Poetry to requirements.txt',
+    )
+
+    check_cmd = ('poetry', 'export', '--without-hashes')
+    build_cmd = ('poetry', 'export', '--output', 'requirements.txt', '--without-hashes')
+
+    # check current file
+    proc = subprocess.Popen(check_cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+
+    stdout, stderr = proc.communicate()
+
+    stdout = stdout.decode()
+
+    retcode = 0
+
+    if open("requirements.txt", "r").read() != stdout:
+        subprocess.Popen(build_cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        print('created new requirements.txt file')
+        retcode = 1
+    return retcode
+
+
+if __name__ == '__main__':
+    exit(main())
