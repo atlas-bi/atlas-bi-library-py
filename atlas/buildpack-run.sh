e1db89162fef1080681ad9f01f2acfc19ecf29a2
Christopher Pickering <cpickering@rhc.net>
2021-05-05 21:25:23 -0500

    changed permission

--- atlas/buildpack-run.sh
+++ atlas/buildpack-run.sh
@@ -1,3 +1,4 @@
+#!/bin/bash
 python manage.py flush --no-input --settings atlas.settings.demo
 python manage.py migrate --run-syncdb  --settings atlas.settings.demo
 python manage.py loaddata index/fixtures/*.yaml --settings atlas.settings.demo
