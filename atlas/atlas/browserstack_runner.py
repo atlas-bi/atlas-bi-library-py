3ef6e7a1cc0f6b33f1aec31c5d9834c743fb992e
Christopher Pickering <cpickering@rhc.net>
2021-05-24 08:30:16 -0500

    moved browserstack to pip

--- atlas/atlas/browserstack_runner.py
+++ atlas/atlas/browserstack_runner.py
@@ -4,7 +4,9 @@ A custom test runner is used so that we can run the tests
 with multiple browser configurations.
 """
 # pylint: disable=C0115
+import os
 
+from browserstack.local import Local
 from django.conf import settings
 from django.test.runner import DiscoverRunner
 
@@ -13,6 +15,11 @@ class BrowserStackDiscoverRunner(DiscoverRunner):
     def run_suite(self, suite, **kwargs):
         """Override run_suite with a for loop function."""
         suite._cleanup = False
+        bs_local = Local()
+        bs_local_args = {"forcelocal": "true"}
+        bs_local.start(**bs_local_args)
+        print(bs_local.isRunning())  # noqa: T001
+
         result = {}
         for cap in getattr(settings, "BROWSERSTACK_CAPS", {}):
             settings.WEBDRIVER = cap
@@ -21,4 +28,5 @@ class BrowserStackDiscoverRunner(DiscoverRunner):
             result = runner.run(suite)
 
         # return last result.. not sure how to merge them
+        bs_local.stop()
         return result
