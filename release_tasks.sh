da963a30afc2f58284ded878cbdc8fc562f43e09
Christopher Pickering <cpickering@rhc.net>
2021-06-21 13:48:48 -0500

    fixed python commands

--- release_tasks.sh
+++ release_tasks.sh
@@ -6,9 +6,9 @@ python manage.py loaddata index/fixtures/*.yaml --settings atlas.settings.demo
 # after save events are added, as loaddata triggers those events.
 # will still need to start solr& celery before load data however.
 /opt/solr/bin/solr start -force -noprompt -v \
-&& (DJANGO_SETTINGS_MODULE='atlas.settings.dev' poetry run celery -A atlas worker -l DEBUG -E &) \
+&& (DJANGO_SETTINGS_MODULE='atlas.settings.dev' celery -A atlas worker -l DEBUG -E &) \
 && sleep 5 \
-&& python python  manage.py  shell --command="import atlas; from etl.tasks.search import initiatives; initiatives.reset_initiatives.delay()" \
-&& python python  manage.py  shell --command="import atlas; from etl.tasks.search import projects; projects.reset_projects.delay()" \
-&& python python  manage.py  shell --command="import atlas; from etl.tasks.search import reports; reports.reset_reports.delay()" \
-&& python python  manage.py  shell --command="import atlas; from etl.tasks.search import terms; terms.reset_terms.delay()"
+&& python  manage.py  shell --command="import atlas; from etl.tasks.search import initiatives; initiatives.reset_initiatives.delay()" \
+&& python  manage.py  shell --command="import atlas; from etl.tasks.search import projects; projects.reset_projects.delay()" \
+&& python  manage.py  shell --command="import atlas; from etl.tasks.search import reports; reports.reset_reports.delay()" \
+&& python  manage.py  shell --command="import atlas; from etl.tasks.search import terms; terms.reset_terms.delay()"
