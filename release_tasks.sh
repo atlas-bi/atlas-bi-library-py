2297280959572dadf56bc033292932a60d4286b3
Christopher Pickering <cpickering@rhc.net>
2021-09-16 15:33:35 +0200

    added npm build

--- release_tasks.sh
+++ release_tasks.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 npm install
+npm run build
 python manage.py flush --no-input --settings atlas.settings.demo
 python manage.py migrate --run-syncdb  --settings atlas.settings.demo
 python manage.py loaddata index/fixtures/*.yaml --settings atlas.settings.demo
